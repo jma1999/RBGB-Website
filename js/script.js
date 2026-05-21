e8071f87063a92f04354c88945cbce22c4a83274
jma1999 <179515747+jma1999@users.noreply.github.com>
2026-05-21 18:21:10 -0400

    website v3

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log("RBGB website loaded.");
+const titleLines = document.querySelectorAll(".title-line");
+
+let currentTitle = 0;
+
+function rotateHeroTitle() {
+  titleLines[currentTitle].classList.remove("active");
+
+  currentTitle = (currentTitle + 1) % titleLines.length;
+
+  titleLines[currentTitle].classList.add("active");
+}
+
+setInterval(rotateHeroTitle, 2800);
